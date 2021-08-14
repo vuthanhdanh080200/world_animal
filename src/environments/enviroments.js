11ddcc6e7f44a101be2eb7dcfbf1edd1f26bdd60
Danh <Danh@LAPTOP-FDQSAODR>
2021-08-15 00:02:41 +0700

    deploy3

--- src/environments/enviroments.js
+++ src/environments/enviroments.js
@@ -1,5 +1,5 @@
 export const environments = {
   production: false,
   baseUrl: "https://world-animal.herokuapp.com/api/",
-  basePage: "https://vuthanhdanh080200.github.io/world_animal_frontEnd/",
+  basePage: "/",
 };
